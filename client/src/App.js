7472987ceba399d9826cd4445786fc4aff1f2b77
brijamfitz <brianjfitzgerald@gmail.com>
2019-01-03 20:34:24 -0500

    add NoMatch 404 route page

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,23 @@
 import React from "react";
+import Nav from "./components/Nav";
 import Books from "./pages/Books";
 import SavedBooks from "./pages/SavedBooks";
+import NoMatch from "./pages/NoMatch";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Nav from "./components/Nav";
 
 const App = () => {
-    return (
-      <Router>
-        <div>
-          <Nav />
-          <Switch>
-            <Route exact path="/" component={Books} />
-            <Route exact path="/savedbooks" component={SavedBooks} />
-            {/* <Route exact path="/books/:id" component={Detail} />
-            <Route component={NoMatch} /> */}
-          </Switch>
-        </div>
-      </Router>
-    );
-  }
-  
-  export default App;
+  return (
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Books} />
+          <Route exact path="/savedbooks" component={SavedBooks} />
+          <Route component={NoMatch} />
+        </Switch>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
