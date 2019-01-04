2f4a34385ee628cdbd54373452ea371caecf1a05
brijamfitz <brianjfitzgerald@gmail.com>
2019-01-04 18:58:25 -0500

    remove static.json - add *(get) route to server.js

--- server.js
+++ server.js
@@ -18,6 +18,10 @@ app.use(routes);
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
 // Start the API server
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
